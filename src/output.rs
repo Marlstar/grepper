b26ccf02bd0e0bea5fdf3da4470955eb6b4da08f
Marley Reeves <marley@marlstar.com>
2025-05-29 12:29:37 +1000

    feat: colorise output

--- src/output.rs
+++ src/output.rs
@@ -1,10 +1,14 @@
 use crate::grep::Hit;
 use crate::util::relative_path;
+use colored::Colorize;
 
 pub fn display(hits: Vec<Hit>) {
     for hit in hits {
         let rel_path = relative_path(&*crate::CWD, &hit.file).unwrap();
 
-        println!("[{}:{}] {}", rel_path.display(), hit.line_number, hit.line_content)
+        let file = format!("{}", rel_path.display()).purple();
+        let line = format!("{}", hit.line_number).green();
+        let content = hit.line_content;
+        println!("[{}:{}] {}", file, line, content);
     }
 }
